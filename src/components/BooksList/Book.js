464c54443c8fecf9e32b951d96ff102f62fe0341
Slawek <61154596+Ultran@users.noreply.github.com>
2021-11-02 21:48:07 +0100

    add icons to book list elements

--- src/components/BooksList/Book.js
+++ src/components/BooksList/Book.js
@@ -1,4 +1,8 @@
 import { useRef } from "react";
+import TitleIcon from "@mui/icons-material/Title";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 
 const Book = ({ title, author, publishingHouse, pages }) => {
   const elementRef = useRef(null);
@@ -17,16 +21,22 @@ const Book = ({ title, author, publishingHouse, pages }) => {
         onMouseLeave={() => removeColor()}
       >
         <li>
-          <span>title:</span> {title}
+          <TitleIcon
+            style={{ color: "#E67E22", border: "1px solid #E67E22" }}
+          />{" "}
+          <span>Title:</span> {title}
         </li>
         <li>
-          <span>author:</span> {author}
+          <AccountBoxIcon style={{ color: "#E67E22" }} />
+          <span>Author:</span> {author}
         </li>
         <li>
-          <span>publishing House:</span> {publishingHouse}
+          <MenuBookIcon style={{ color: "#E67E22" }} />
+          <span>Publishing House:</span> {publishingHouse}
         </li>
         <li>
-          <span>pages:</span> {pages}
+          <FormatListNumberedIcon style={{ color: "#E67E22" }} />
+          <span>Pages:</span> {pages}
         </li>
       </ul>
     </>
